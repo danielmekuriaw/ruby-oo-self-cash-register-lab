f9de60c1688b4bb8bf4305537c28457aac15392e
danielmekuriaw <daniel.mekuriaw16@gmail.com>
2021-06-09 22:25:51 -0500

    self cash register lab solution

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,47 @@
+class CashRegister
 
+    attr_accessor :total, :discount
+    @@last_item_price = 0
+
+    def initialize(discount = nil)
+        @total = 0
+        @discount = discount
+        @@item = []
+
+    end
+
+    def add_item(title, price, quantity=1)
+        self.total = self.total + quantity*price
+        @@last_item_price = quantity*price
+        while quantity > 0 do
+            @@item.push(title)
+            quantity = quantity - 1
+        end
+    end
+
+    def apply_discount
+        if discount != nil
+            self.total = self.total - (self.discount*self.total/100.0)
+            "After the discount, the total comes to $#{self.total.to_i}."
+        else
+            "There is no discount to apply."
+        end
+
+    end
+
+    def items
+        @@item
+    end
+
+    def void_last_transaction
+        if @@item != nil
+            self.total = self.total - @@last_item_price
+            @@item.pop
+
+        else
+            self.total = 0
+        end
+    end
+
+
+end
